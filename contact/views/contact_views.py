4cd3cad7a0d00061c286cab5f1939b155a32c283
adalbertobengaly <adalbertobengaly@gmail.com>
2023-11-19 20:50:16 -0300

    Filtrando valores com Q e OR para o campo de pesquisa

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.db.models import Q
 from contact.models import Contact
 
 
@@ -10,6 +11,47 @@ def index(request):
     context = {
         'contacts': contacts,
         'site_title': 'Contatos - '
+    }
+
+    return render(
+        request,
+        'contact/index.html',
+        context,
+    )
+
+
+def search(request):
+    search_value = request.GET.get('q', '').strip()
+
+    if search_value == '':
+        return redirect('contact:index')
+
+    # https://docs.djangoproject.com/en/4.2/ref/models/querysets/#field-lookups
+    filters = (
+        Q(first_name__icontains=search_value) |
+        Q(last_name__icontains=search_value) |
+        Q(phone__icontains=search_value) |
+        Q(email__icontains=search_value)
+    )
+
+    try:
+        first_name, last_name = search_value.split(maxsplit=1)
+        filters |= Q(
+            Q(first_name__icontains=first_name) &
+            Q(last_name__icontains=last_name)
+        )
+    except ValueError:
+        pass
+
+    contacts = Contact.objects \
+        .filter(show=True) \
+        .filter(filters) \
+        .order_by('-id')
+
+    context = {
+        'contacts': contacts,
+        'site_title': 'Search - ',
+        'search_value': search_value,
     }
 
     return render(

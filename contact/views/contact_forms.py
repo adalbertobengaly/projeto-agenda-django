e3e43da1e9fc043a68adb5aa5e39227acf4a0a71
adalbertobengaly <adalbertobengaly@gmail.com>
2023-11-20 14:04:16 -0300

    Criando a URL, view e template para criar um contato (CREATE) e partial _pagination.html corrigido

---
+++ contact/views/contact_forms.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from django.core.paginator import Paginator
+from django.db.models import Q
+from contact.models import Contact
+
+
+def create(request):
+
+    context = {
+        'site_title': 'Contato - '
+    }
+
+    return render(
+        request,
+        'contact/create.html',
+        context,
+    )
